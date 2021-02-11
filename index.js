4868230449f6d6eb5f3e382841de35b90c3d5b12
Alicia Weenum <weenum.alicia@gmail.com>
2021-02-11 14:15:19 -0500

    commit tree 133c257a6647f93105b209594ec056ca69a9b415

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Add your functions here
+
+function map(src, x){
+    let r = []
+    for (let i = 0; i < src.length; i++ ) {
+        map([1, 2, 3, -9], function(a){ return a * a })
+      
+      }
+      return r
+}
+
+// let mapArr = src.map(function(x){
+//     return x
+// })
